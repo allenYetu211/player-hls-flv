0c7e366db46c1bb2458f2d1f390420a13568d56d
AllenOYang <ouyangzhou@xylink.com>
2021-01-14 15:09:09 +0800

    feat: add story books

---
+++ src/stories/vp.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import VideoPlayer from '../../dist';
+
+export interface initConfig {
+  type: 'flv' | 'hls' | 'mp4' | 'm3u8' | 'dash',
+  src?: string;
+  autoplay?: boolean;
+  option?: {
+    multiStreams: {
+      src: string;
+      text: string
+    }[];
+    playIndex: number;
+  };
+  isMobile?: boolean;
+  hideControl?: boolean;
+  hideMultiCode?: boolean;
+  hideRefresh?: boolean;
+  hideMultiple?: boolean;
+  hideProgressBar?: boolean;
+  onVideoPlayerState?: (vp: any) => void;
+  multiple?: IMultiple;
+  vod?: boolean;
+  poster?: string;
+  duration?: number;
+  thumbnail?: {
+    picture: string;
+    width?: number;
+    height?: number;
+    count: number;
+    rowCount?: number;
+    backgroundSize: number;
+  };
+}
+
+
+export interface IMultiple {
+  list: {
+    text: string,
+    value: number,
+  }[],
+  initIndex: number
+}
+
+
+export interface IMultiStreams {
+  src: string;
+  text: string
+}
+
+export interface IMultiStreamsContainer {
+  multiStreams: IMultiStreams[];
+  playIndex: number;
+}
+
+
+export const VideeoPlayer: React.FC<initConfig> = (props) => {
+  const onVideoPlayerState = (vp: any) => {
+    // console.log('vp', vp.destroy);
+    // console.log('vp==>>>>', vp.videoEl.playbackRate = 1)
+  }
+
+
+  return (
+    <VideoPlayer {...props} />
+  )
+}
