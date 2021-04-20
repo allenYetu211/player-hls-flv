10c25257800663d60846de66c1939efffca282b7
AllenOYang <ouyangzhou@xylink.com>
2021-04-20 22:45:54 +0800

    style: change config

--- src/player/flv-player.ts
+++ src/player/flv-player.ts
@@ -2,17 +2,17 @@
  * @Author: Allen OYang
  * @Date: 2021-03-30 10:59:36
  * @Descripttion: 
- * @LastEditTime: 2021-04-19 08:54:23
+ * @LastEditTime: 2021-04-20 21:43:09
  * @FilePath: /ts-vp/src/player/flv-player.ts
  */
 import VideoControl from './video-control';
 import {videoConfig, IMultiStreams} from '@interfaces/index';
 
-// import flvjs from 'flv.js';
+import flvjs from 'flv.js';
 
 // @ts-ignore
 // IE 11 测试环境需要使用本行代码
-import flvjs from 'flv.js/dist/flv';
+// import flvjs from 'flv.js/dist/flv';
 
 export default class FlvPlayer extends VideoControl {
 
